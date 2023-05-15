a834d80a000c4ef057221363b72c22e7616d6c05
git/Isabela Cartaxo <belacartaxo@gmail.com>
2023-05-14 21:26:14 -0300

    added content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { FiSearch } from "react-icons/fi"
+import "./style.css"
+
 function App() {
   return (
-    <div className="App">
-      <h1>Teste projeto</h1>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+      <div className="containerInput">
+        <input 
+        type="text" 
+        placeholder="Digite o seu CEP"
+        />
+        <button className="buttonSearch">
+          <FiSearch size="25" color="#FFF"/>
+        </button>
+      </div>
+
+      <main className="main">
+        <h2>CEP: 41830532</h2>
+
+        <span>Rua teste</span>
+        <span>Complemento: alguma coisa</span>
+        <span>Pituba</span>
+        <span>Campo grande -MS</span>
+      </main>
     </div>
   );
 }
