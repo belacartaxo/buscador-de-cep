6fb4c28d277dfabbe19fe6ac777ddf2312ca4dcb
git/Isabela Cartaxo <belacartaxo@gmail.com>
2023-05-14 22:43:58 -0300

    finished project

--- src/App.js
+++ src/App.js
@@ -29,6 +29,14 @@ function App() {
     }
   }
 
+  function handleSearchKeyUp(key){
+    if(key === 'Enter'){
+      handleSearch();
+    }
+  }
+
+
+
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
@@ -38,20 +46,23 @@ function App() {
         placeholder="Digite o seu CEP"
         value={input}
         onChange={(e) => setInput(e.target.value)} //toda vez que algo é digitado o set input é chamado e o valor digitado é passado para a função
+        onKeyDown={(e) => handleSearchKeyUp(e.key)}
         />
         <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size="25" color="#FFF"/>
         </button>
       </div>
+      
+      {Object.keys(cep).length > 0 && ( // se o cep tiver pelo menos um elemento o main irá aparecer
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
 
-      <main className="main">
-        <h2>CEP: 41830532</h2>
-
-        <span>Rua teste</span>
-        <span>Complemento: alguma coisa</span>
-        <span>Pituba</span>
-        <span>Campo grande -MS</span>
-      </main>
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+        </main>
+      )}
     </div>
   );
 }
