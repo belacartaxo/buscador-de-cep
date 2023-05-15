18f44554180d48f09f507362e479f21d4affb9a4
git/Isabela Cartaxo <belacartaxo@gmail.com>
2023-05-14 22:24:14 -0300

    making cep request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { useState } from "react"
 import { FiSearch } from "react-icons/fi"
+import api from "./services/api"
 import "./style.css"
 
 function App() {
+
+  /*
+    input - NOME DO ESTADO
+    setInput - FUNÇÃO QUE TROCA O VALOR DO ESTADO
+  */
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
+
+  async function handleSearch(){
+    //01310930/json/
+    if(input === ''){
+      alert("Preencha algum CEP!");
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput("");
+    } catch {
+      alert("[ERRO!]");
+      setInput("");
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
@@ -9,8 +36,10 @@ function App() {
         <input 
         type="text" 
         placeholder="Digite o seu CEP"
+        value={input}
+        onChange={(e) => setInput(e.target.value)} //toda vez que algo é digitado o set input é chamado e o valor digitado é passado para a função
         />
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size="25" color="#FFF"/>
         </button>
       </div>
